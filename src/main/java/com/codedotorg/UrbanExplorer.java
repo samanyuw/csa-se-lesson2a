c00b1dfc7e14907c02d03f8b8dc34bcdedf1653b
samanyuw <swarhade0001@mymail.lausd.net>
2024-05-24 22:03:11 -0700

    updated sortCities() method

--- src/main/java/com/codedotorg/UrbanExplorer.java
+++ src/main/java/com/codedotorg/UrbanExplorer.java
@@ -1,6 +1,8 @@
 package com.codedotorg;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -93,16 +95,24 @@ public class UrbanExplorer {
 
         if (!newCity.isEmpty()) {
             cities.add(new City(newCity));
+            sortCities();
             refreshList();
             inputField.clear();
+
         }
+
     }
 
     /**
      * Sorts the cities in the 'cities' list based on their names.
      */
     public void sortCities() {
-        
+        Collections.sort(cities, new Comparator<City>() {
+        @Override
+        public int compare(City c1, City c2) {
+            return c1.getName().compareTo(c2.getName());
+        }
+        });
 
     }
 
@@ -134,5 +144,6 @@ public class UrbanExplorer {
 
         return tempButton;
     }
+    
 
 }
